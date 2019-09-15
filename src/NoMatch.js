d86fe6fe31d99c98738f64f77ab55c7830029f5f
yuehaowang <wangyuehao1999@gmail.com>
2019-09-15 20:06:57 +0800

    Added 404 page.

---
+++ src/NoMatch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function NoMatch() {
+    return (
+        <div className="uk-flex uk-flex-center uk-margin-large-top">
+            <div className="uk-text-center">
+                <h1>404 Not Found</h1>
+                <p className="uk-text-lead">抱歉，此见面尚未开放</p>
+            </div>
+        </div>
+    );
+}
+
+export default NoMatch;
