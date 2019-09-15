c8b587efc0c6ceac532c98033356bad82988c8ae
yuehaowang <wangyuehao1999@gmail.com>
2019-09-15 17:45:43 +0800

    Beautify footer.

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="uk-margin-medium-top uk-grid-divider uk-box-shadow-medium uk-child-width-expand@s uk-background-muted uk-padding" uk-grid="true">
+        <div className="uk-margin-small-top uk-grid-divider uk-box-shadow-medium uk-child-width-expand@s uk-background-muted uk-padding-large" uk-grid="true">
             <div>
-                <h4>网站功能</h4>
+                <h4><span uk-icon="icon: info;"></span> 网站功能</h4>
                 <ul>
                     <li>海量的频道信息，多维度的频道能力指标</li>
                     <li>基于梅卡夫斯定律与传统现金流折现的估值模型</li>
@@ -14,13 +14,13 @@ function Footer() {
                 </ul>
             </div>
             <div>
-                <h4>合作伙伴</h4>
+                <h4><span uk-icon="icon: users;"></span> 合作伙伴</h4>
                 <ul>
                     <li>BiliBili频道主 狸子LePtC</li>
                 </ul>
             </div>
             <div>
-                <h4>联系我们</h4>
+                <h4><span uk-icon="icon: receiver;"></span> 联系我们</h4>
                 <ul>
                     <li>站长邮箱：wangzy7@shanghaitech.edu.cn</li>
                     <li>技术支持：wangph@shanghaitech.edu.cn</li>
