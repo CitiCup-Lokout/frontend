813b5528317ed2cb6708827421309128e829e338
yuehaowang <wangyuehao1999@gmail.com>
2019-09-15 21:35:47 +0800

    Fixed typo in 404 page.

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -6,7 +6,7 @@ function NoMatch() {
         <div className="uk-flex uk-flex-center uk-margin-large-top">
             <div className="uk-text-center">
                 <h1>404 Not Found</h1>
-                <p className="uk-text-lead">抱歉，此见面尚未开放</p>
+                <p className="uk-text-lead">抱歉，此界面尚未开放</p>
             </div>
         </div>
     );
