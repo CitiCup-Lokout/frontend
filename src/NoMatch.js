a97b0139ea71f04be56cee7e47627f89c0cc4c9c
yuehaowang <wangyuehao1999@gmail.com>
2019-10-05 00:31:12 +0800

    Updated no match page.

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import ImgOops from './res/oops.svg';
+
 
 function NoMatch() {
     return (
         <div className="uk-flex uk-flex-center uk-margin-large-top">
-            <div className="uk-text-center">
-                <h1>404 Not Found</h1>
-                <p className="uk-text-lead">抱歉，此界面尚未开放</p>
+            <div>
+                <h1><img src={ImgOops} /></h1>
+                <p className="uk-text-lead">您要访问的页面还没有准备好！</p>
+                <p>可能的原因是...</p>
+                <ul>
+                    <li>404 Not Found</li>
+                    <li>经营性备案尚未通过</li>
+                    <li>该功能还在测试中</li>
+                    <li>您的权限不足</li>
+                </ul>
             </div>
         </div>
     );
